14cb63b74b1c398e7fdef75cad6fb120d6e16670
abimanyu-4123005 <abimanyupujiandikamaryadi@saintek.unipdu.ac.id>
2024-12-04 16:06:08 +0700

    commit pertama

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+ <style>
+ </style>
+</head>
+
+<body>
+ <h1>Sitem Informasi Sekolah</h1>
+ <h2> Smk Unipdu </h2>
+ <a href="data_siswa.php">Daftar siswa </a><br>
+ <a href="tambah_siswa.php">Pendaftaran</a>
+</body>
+
+</html>
